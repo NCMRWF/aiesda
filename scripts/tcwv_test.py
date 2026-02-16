03b16d80079cc29a387f9726311f299001672a4f
Gibies George <gibiesgeorge@gmail.com>
2026-02-16 15:23:18 +0530

    Initialize environment variables and system paths
    Add environment variable handling and path setup for user, libraries, and dictionaries.

--- scripts/tcwv_test.py
+++ scripts/tcwv_test.py
@@ -1,1 +1,13 @@
 
+
+import os,sys
+
+USER=os.environ.get('USER',"myhome")
+CURR_PATH=os.path.dirname(os.path.abspath(__file__))
+PKGHOME=os.path.dirname(CURR_PATH)
+OBSLIB=os.environ.get('OBSLIB',PKGHOME+"/pylib")
+sys.path.append(OBSLIB)
+OBSDIC=os.environ.get('OBSDIC',PKGHOME+"/pydic")
+sys.path.append(OBSDIC)
+OBSNML=os.environ.get('OBSNML',PKGHOME+"/nml")
+sys.path.append(OBSNML)
